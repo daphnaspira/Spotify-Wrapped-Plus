e894fb7f413abe379915ecbab05e842e706acde2
daphnaspira <86253661+daphnaspira@users.noreply.github.com>
2022-01-12 22:11:24 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,22 +1,17 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def get_history(filename):
+def get_listening_history(filename):
     """
     filename: (str) the path to the .json file containing the spotify listening history
-    loads in the listening history as a pandas dataframe
-    """
-    df = pd.read_json(filename)
-    return df
-
-def date_formatting(df):
-    """
-    df = (dataframe) the name of the dataframe with the listening history
-    adds columns to the dataframe based on the information in the 'endTime' column:
+    
+    Loads in the listening history as a pandas dataframe
+    Adds columns to the dataframe based on the information in the 'endTime' column of the df:
     'Date' contains the year-month-day date that the song was played
     'Time' contains the hour-minute-second time that the song finished playing
     'Month-Year' contains the year-month date that the song was played
     """
+    df = pd.read_json(filename)
     df['Date'] = pd.to_datetime(df['endTime']).dt.date
     df['Time'] = pd.to_datetime(df['endTime']).dt.time
     df['Month-Year'] = pd.to_datetime(df['endTime']).dt.to_period('m')
@@ -27,7 +22,8 @@ def song_play_frequencies(df, dates, date_column_name):
     df = (dataframe) the name of the dataframe with the listening history
     dates = (list) the range of periods to use (days or months)
     date_column_name = (str) the name of the time period column to use ('Date' or 'Month-Year')
-    returns a new dataframe with the number of times each song was played within the time period specified (each day or month)
+    
+    Returns a new dataframe with the number of times each song was played within the time period specified (each day or month)
     """
     song_freqs_df = pd.DataFrame()
     for date in dates:
@@ -41,6 +37,7 @@ def merge_songs(song_to_merge, original_song):
     """
     song_to_merge = (str) the alternate name for the song (will be replaced)
     original song = (str) the name of the song that you want to display (what to replace with)
+    
     Use if the same song exists in two different versions on Spotify and is being counted separately but you want them to be counted as one
     e.g. the original version and extended version of a song
     """
@@ -52,6 +49,7 @@ def print_top_songs_per_month(df, months, n=5):
     df = (dataframe) the name of the dataframe with the listening history
     months = (list) the range of months to use
     n (optional) = (int) the number of top songs to display for each month. default is set to 5.
+    
     Prints out the top n songs for each month with the name of the song, name of the artist, and the number of times the song was played in that month.
     """
     for month in months:
@@ -61,7 +59,7 @@ def print_top_songs_per_month(df, months, n=5):
         print(freq.head(n))
         print()
         
-def top_songs(df, months, n=5):
+def top_songs_per_month(df, months, n=5):
     """
     df = (dataframe) the name of the dataframe with the listening history
     months = (list) the range of months to use
